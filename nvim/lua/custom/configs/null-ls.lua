5ac4dff8c783dbef2462ea2e24b71dea3c08e883
Milo Galli <42500999+thaMilo@users.noreply.github.com>
2023-12-30 00:38:43 +0100

    Add files via upload

--- nvim/lua/custom/configs/null-ls.lua
+++ nvim/lua/custom/configs/null-ls.lua
@@ -1,25 +1,25 @@
--- local null_ls = require "null-ls"
--- local b = null_ls.builtins
---
--- local sources = {
---   -- webdev stuff
---   b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
---   b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
---   -- python3
---   b.formatting.black,
---   b.diagnostics.ruff,
---   b.diagnostics.mypy,
---   -- Lua
---   b.formatting.stylua,
---   -- Go  
---   b.formatting.gofumpt,
---   b.formatting.goimports_reviser,
---   -- cpp
---   b.formatting.clang_format,
--- }
---
--- null_ls.setup {
---   debug = true,
---   sources = sources,
---   autostart = true,
--- }
+local null_ls = require "null-ls"
+local b = null_ls.builtins
+
+local sources = {
+  -- webdev stuff
+  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  -- python3
+  b.formatting.black,
+  b.diagnostics.ruff,
+  b.diagnostics.mypy,
+  -- Lua
+  b.formatting.stylua,
+  -- Go  
+  b.formatting.gofumpt,
+  b.formatting.goimports_reviser,
+  -- cpp
+  b.formatting.clang_format,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  autostart = true,
+}
--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -8,7 +8,7 @@ local plugins = {
     dependencies = {
       -- format & linting
       {
-        "jose-elias-alvarez/null-ls.nvim",
+        "nvimtools/none-ls.nvim",
         config = function()
           require "custom.configs.null-ls"
         end,
@@ -52,13 +52,7 @@ local plugins = {
   },
   {
     "zbirenbaum/copilot.lua",
-    -- Lazy load when event occurs. Events are triggered
-    -- as mentioned in:
-    -- https://vi.stackexchange.com/a/4495/20389
     event = "InsertEnter",
-    -- You can also have it load at immediately at
-    -- startup by commenting above and uncommenting below:
-    -- lazy = false
     opts = overrides.copilot,
   },
 }
