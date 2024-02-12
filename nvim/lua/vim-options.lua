bb48df38fc659292daae7f720cef1ed1fb74febc
thaMilo <itsmemjlo@protonmail.com>
2024-02-12 21:36:54 +0100

    modified some things

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,23 +4,20 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 
--- Navigate vim panes better
-vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
-vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
-vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+  -- Navigate vim panes better
+  vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+  vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+  vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+  vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
--- Save current file with ctrl-s
-vim.keymap.set('n', '<c-s>', ':w<CR>')
+  -- Save current file with leader s
+  vim.keymap.set('n', '<leader>s', ':w<CR>')
+  vim.keymap.set('n', '<c-s>', ':w<CR>')
 
 -- Comment line with leader 7
 vim.keymap.set('n', '<leader>7', ':s/^/#<CR>')
 -- Uncomment line with leader 8
 vim.keymap.set('n', '<leader>8', ':s/^#//<CR>')
-
--- the same thing but for visual mode
-vim.keymap.set('v', '<leader>7', ':s/^/#<CR>')
-vim.keymap.set('v', '<leader>8', ':s/^#//<CR>')
 
 -- move the cursor up and down 5 lines with ctrl-j and ctrl-k
 vim.keymap.set('n', '<c-j>', '5j')
