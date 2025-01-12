536358241fd22053c8ca91d54d16f3d1a0752804
thaMilo <milougo.galli@gmail.com>
2025-01-12 20:01:13 +0100

    working on it

--- wallpapers/wall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-wallpath="/home/milo/Desktop/milo/thispc/dotfiles/wallpapers/arch.png"
-
-curl -o sub.jpg $1
-magick -size 2880x1800 xc:"#1e1e2e" base.png 
-magick sub.jpg -bordercolor "#BDBEDE" -border 10x10 sub.jpg
-magick base.png sub.jpg -gravity north -geometry +0+500  -composite "$wallpath"
-rm base.png sub.jpg
