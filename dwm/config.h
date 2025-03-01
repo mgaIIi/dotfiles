f8b11b0cafb7eeed762b3c9171f190849522c183
thaMilo <milougo.galli@gmail.com>
2025-03-01 13:28:44 +0100

    working on it

--- dwm/config.h
+++ dwm/config.h
@@ -1,6 +1,3 @@
-/* See LICENSE file for copyright and license details. */
-
-/* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -17,7 +14,6 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
-
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
@@ -59,18 +55,30 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char *wallpaper_toggle[] = { "feh", "--bg-fill", "/home/milo/Pictures/wallpapers/void.png", NULL};
+static const char *browser_command[] = { "firefox", NULL};
+
+/* patching */
+#include "patches/shiftview.c"
+
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_w,      spawn,          {.v = wallpaper_toggle}},
+	{ MODKEY,                       XK_i,      spawn,          {.v = browser_command}},
+
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+
 	{ MODKEY,                       XK_h,      focusstack,     {.i = +1} },
 	{ MODKEY,                       XK_l,      focusstack,     {.i = -1} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +2} },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -2} },
+
+	{ MODKEY,                       XK_n,      shiftview,     {.i = +1} },
+	{ MODKEY,                       XK_b,      shiftview,     {.i = -1} },
+
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
