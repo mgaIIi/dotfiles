5f733da06a30fc6a6a5124e0f9c8dcc6242c5905
thaMilo <milougo.galli@gmail.com>
2025-03-01 12:03:43 +0100

    working on it

--- dwm/config.h
+++ dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -67,6 +67,10 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_w,      spawn,          {.v = wallpaper_toggle}},
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY,                       XK_h,      focusstack,     {.i = +1} },
+	{ MODKEY,                       XK_l,      focusstack,     {.i = -1} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +2} },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -2} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
