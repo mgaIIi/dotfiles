2cbc6ce9d1bfa2a7fa3b898db70bb9c1d2c880ba
mgaIIi <milougo.galli@gmail.com>
2025-03-08 02:09:23 +0100

    working on it

--- dwm/config.h
+++ dwm/config.h
@@ -1,28 +1,24 @@
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Agave Nerd Font:size=11" };
 static const char dmenufont[]       = "Agave Nerd Font:size=11";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#11111b";
+static const char col_gray2[]       = "#1e1e2e";
+static const char col_gray3[]       = "#45475a";
+static const char col_gray4[]       = "#a6adc8";
+static const char col_cyan[]        = "#f9e2af";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
 };
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 };
@@ -54,8 +50,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *wallpaper_toggle[] = { "feh", "--bg-fill", "/home/milo/Pictures/wallpapers/void.png", NULL};
+static const char *termcmd[]  = { "st", NULL };
+static const char *wallpaper_toggle[] = { "feh", "--bg-fill", "/home/mgalli/Pictures/wallpapers/void.png", NULL};
 static const char *browser_command[] = { "firefox", NULL};
 
 /* patching */
