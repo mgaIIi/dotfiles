610e2ee676bcf1e0343341f487f23de5f0ac9172
mgaIIi <milougo.galli@gmail.com>
2025-03-16 17:13:34 +0100

    autopush

--- dwm/config.h
+++ dwm/config.h
@@ -1,19 +1,19 @@
-static const unsigned int borderpx = 4; /* border pixel of windows */
-static const unsigned int gappx = 20;    /* gaps between windows */
+static const unsigned int borderpx = 2; /* border pixel of windows */
+static const unsigned int gappx = 0;    /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const int showbar = 1;           /* 0 means no bar */
+static const int showbar = 0;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"JetbrainsMono NF:size=13"};
-static const char dmenufont[] = "JetbrainsMono NF:size=13";
+static const char *fonts[] = {"Agave Nerd Font:size=14"};
+static const char dmenufont[] = "Agave Nerd Font:size=14";
 static const char col_gray1[] = "#11111b";
 static const char col_gray2[] = "#1e1e2e";
 static const char col_gray3[] = "#45475a";
 static const char col_gray4[] = "#a6adc8";
-static const char col_cyan[] = "#313244";
+static const char col_cyan[] = "#aaaaaa";
 static const char *colors[][3] = {
     /*               fg         bg         border   */
     [SchemeNorm] = {col_gray4, col_gray1, col_gray2},
-    [SchemeSel] = {col_gray4, col_cyan, col_cyan},
+    [SchemeSel] = {col_gray1, col_cyan, col_cyan},
 };
 /* tagging */
 static const char *tags[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
@@ -56,14 +56,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
-    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
+    "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb", col_gray1,
+    "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray1, NULL};
 static const char *termcmd[] = {"st", NULL};
 static const char *wallpaper_toggle[] = {
     "feh", "--bg-fill", "/home/mgalli/Pictures/wallpapers/void.png", NULL};
 static const char *battify[] = {"/home/mgalli/Scripts/battify.sh", NULL};
 static const char *dattify[] = {"/home/mgalli/Scripts/dattify.sh", NULL};
 static const char *browser_command[] = {"firefox", NULL};
+static const char *screenshot[] = {"flameshot", "gui", NULL};
 
 /* patching */
 #include "patches/shiftview.c"
@@ -83,7 +84,9 @@ static const Key keys[] = {
     {MODKEY | ShiftMask, XK_equal, setgaps, {.i = 0}},
 
     {MODKEY | ShiftMask, XK_b, spawn, {.v = battify}},
-    {MODKEY | ShiftMask, XK_b, spawn, {.v = dattify}},
+    {MODKEY | ShiftMask, XK_d, spawn, {.v = dattify}},
+    {MODKEY | ShiftMask, XK_s, spawn, {.v = screenshot}},
+
     
     {MODKEY, XK_h, focusstack, {.i = +1}},
     {MODKEY, XK_l, focusstack, {.i = -1}},
@@ -92,6 +95,7 @@ static const Key keys[] = {
 
     {MODKEY, XK_n, shiftview, {.i = +1}},
     {MODKEY, XK_b, shiftview, {.i = -1}},
+
 
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
