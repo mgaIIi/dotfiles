f8d51d1b8c8c80f913f09114047d0024a032f119
mgaIIi <milougo.galli@gmail.com>
2025-10-13 14:17:10 +0200

    autopush

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka NF:pixelsize=18:antialias=true:autohint=true";
+static char *font = "Iosevka NF:pixelsize=23:antialias=true:autohint=true";
 static int borderpx = 10;
 
 /*
