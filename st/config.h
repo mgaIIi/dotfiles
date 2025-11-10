48fae5067958951e579780d418c67a3b1d2c906d
mgaIIi <milougo.galli@gmail.com>
2025-11-10 17:47:50 +0100

    autopush

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka NF:pixelsize=23:antialias=true:autohint=true";
-static int borderpx = 10;
+static char *font = "JetbrainsMono NF:pixelsize=23:antialias=true:autohint=true";
+static int borderpx = 15;
 
 /*
  * What program is execed by st depends of these precedence rules:
